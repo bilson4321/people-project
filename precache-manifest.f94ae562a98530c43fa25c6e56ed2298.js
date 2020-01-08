7fed73da4ea7bbc5dca0bb4767091aff1a22af46
Lonewolf5432 <bilsonc47@gmail.com>
2020-01-08 13:08:24 +0545

    Updates

---
+++ precache-manifest.f94ae562a98530c43fa25c6e56ed2298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99384ace55aaa99e93a0dd6520c0798",
+    "url": "/people-project/index.html"
+  },
+  {
+    "revision": "4ecf9c25c34ca613e239",
+    "url": "/people-project/static/css/main.3350cb14.chunk.css"
+  },
+  {
+    "revision": "b77a6fd4f2d157b3eb6d",
+    "url": "/people-project/static/js/2.b607c6b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/people-project/static/js/2.b607c6b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ecf9c25c34ca613e239",
+    "url": "/people-project/static/js/main.ff6074c8.chunk.js"
+  },
+  {
+    "revision": "ca086fba6f973ecf3c29",
+    "url": "/people-project/static/js/runtime-main.15ecd777.js"
+  }
+]);
